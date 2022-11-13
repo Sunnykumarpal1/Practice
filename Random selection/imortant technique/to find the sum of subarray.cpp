8e2f5bcb2283c60a65aa1ee1f39734a3f1a904a9
Sunny Kumar Pal <111234635+Sunnykumarpal1@users.noreply.github.com>
2022-11-13 14:11:27 +0530

    Create to find the sum of subarray.cpp

---
+++ Random selection/imortant technique/to find the sum of subarray.cpp
@@ -0,0 +1,3 @@
+//  just (n-i)*(i+1) is contribution to right elements and contribution to left elements
+//  and multiply with elements and sum of this will give the sum of all the subarray at O(n)
+      
